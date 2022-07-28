ecc8867847d6c1da40857d700282f885bdad5e5d
Michał J. Gajda <mjgajda@migamake.com>
2022-07-28 02:48:45 +0200

    Benchmarks.

--- less-arbitrary-benchmark/test/Spec.hs
+++ less-arbitrary-benchmark/test/Spec.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+module Main where
+
 import Criterion.Main
 
 import Control.Monad
+import Control.Monad.Par.Class
+
 -- For generic derivations:
 import GHC.Generics
 
@@ -17,10 +21,14 @@ import Test.LessArbitrary
 import Test.Feat
 import Test.Feat.Enumerate
 import Test.Feat.Modifiers
+import Test.Feat.Access
+import Control.Enumerable
 
-data Tree = Branch Tree Tree
-          | Leaf   Int
+data Tree =
+            Leaf   Int
+          | Branch Tree Tree
   deriving (Show, Eq, Ord, Generic)
+-- FIXME: Performance may depend on the order of constructors?
 
 instance Enumerable Tree where
   enumerate =
@@ -33,6 +41,8 @@ instance Enumerable Tree where
 instance QC.Arbitrary Tree where
   arbitrary = tree
   shrink    = QC.genericShrink
+
+instance NFData Tree where
 
 -- | Sized generator of arbitrary for recursive datatype.
 --   From the tutorial: http://www.cse.chalmers.se/~rjmh/QuickCheck/manual.html
@@ -51,9 +61,43 @@ treeSize (Branch a b) = treeSize a + treeSize b
 
 instance LessArbitrary () Tree
 
+testVectorSize = 1000
+testSize = 1000
+
+sampleFeat :: Int -> QC.Gen Tree
+sampleFeat aSize = selectTree aSize <$> QC.choose (c `div` 2, c)
+  where
+    c = count aSize
+    count n = fst $ treeValues !! n
+
+    enumTrees :: Enumerate Tree
+    enumTrees = global
+
+    selectTree = selectWith enumTrees
+
+    treeValues :: [(Integer, [Tree])]
+    treeValues = valuesWith enumTrees
+
 main :: IO ()
 main = defaultMain [
          bgroup "tree" [
+           bench "arbitrary" $
+             nfIO            $
+             QC.generate     $
+             QC.vectorOf testVectorSize $
+             (QC.arbitrary :: QC.Gen Tree)
+         , bench "lessArbitrary" $
+             nfIO            $
+             QC.generate     $
+             QC.vectorOf testVectorSize $
+             sizedCost       $
+             (lessArbitrary :: CostGen () Tree)
+         , bench "feat"      $
+             nfIO            $
+             QC.generate     $
+             QC.vectorOf testVectorSize $
+             QC.sized        $
+             sampleFeat
          ]
        ]
 
